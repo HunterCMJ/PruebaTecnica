ee588883c68db4f64eae021dae289edccb7a6092
HunterCMJ <your.email@example.com>
2026-04-05 02:40:26 +0200

    parte2-upload to gcp-bigquery

--- prueba.py
+++ prueba.py
@@ -1,9 +1,13 @@
+from google.cloud import bigquery
+
 import requests
+
 
 class ApiDownloader:
     # as i get to choose the api, i'll define the url as a class parameter, bc it won't change.
     def __init__(self, url):
         self.url = url
+    
     
     def download_data(self, limit=100):
         response = requests.get(self.url)
@@ -15,6 +19,26 @@ class ApiDownloader:
             return data[:limit]
         else:
             raise TypeError("API response must be a list of records")
+        
+
+class BigQueryUploader:
+    # same logic as above, the project, dataset and table ids are parameters of the class, bc they won't change during the execution.
+    def __init__(self, project_id, dataset_id, table_id):
+        self.client = bigquery.Client(project=project_id)
+        self.table_id = f"{project_id}.{dataset_id}.{table_id}"
+
+    def upload_data(self, data):
+        # We assume 'data' is a list of dictionaries, json like records.
+        # Get the table (we manually created it in the GCP console)
+        table = self.client.get_table(self.table_id)
+        
+        # Insert the data, use the returned errors to check if the upload was successful
+        errors = self.client.insert_rows_json(table, data)
+        if errors:
+            print("Errors inserting rows:", errors)
+        else:
+            print("Data uploaded successfully to BigQuery.")
+
 
 
 if __name__ == "__main__":
@@ -24,6 +48,11 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/comments"
     limit = 100
 
+    # GCP
+    projectid = "project-fec042d1-2105-4fee-851"
+    datasetid = "dataset_pruebatecnica"
+    tableid = "SANDBOX_PruebaTecnica"
+
     ######### Instances #########
 
     downloader = ApiDownloader(url)
@@ -32,4 +61,8 @@ if __name__ == "__main__":
 
     # as the test suggests, ill download only 100 records
     data = downloader.download_data(limit)
+
+    # Upload to BigQuery 
+    uploader = BigQueryUploader(projectid, datasetid, tableid)
+    uploader.upload_data(data)
    
